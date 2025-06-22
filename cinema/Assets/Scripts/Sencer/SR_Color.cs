03d894f66f36f39380585743f6ae07af0b7dd6c7
otakumi-bekon <otatakumi2004@gmail.com>
2025-06-22 15:03:30 +0900

    a

--- cinema/Assets/Scripts/Sencer/SR_Color.cs
+++ cinema/Assets/Scripts/Sencer/SR_Color.cs
@@ -1,29 +1,29 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class SR_Color : MonoBehaviour
-{
+// public class SR_Color : MonoBehaviour
+// {
     
-    public SH_Color serialHandler;
-    public int color;
+//     public SH_Color serialHandler;
+//     public int color;
 
-    void Start()
-    {
-        serialHandler.OnDataReceived += OnDataReceived;
-    }
+//     void Start()
+//     {
+//         serialHandler.OnDataReceived += OnDataReceived;
+//     }
 
-    void Update()
-    {
-        if (!serialHandler.IsOpen)
-        {
-            Debug.LogWarning("Serial port is not open.");
-        }
-    }
+//     void Update()
+//     {
+//         if (!serialHandler.IsOpen)
+//         {
+//             Debug.LogWarning("Serial port is not open.");
+//         }
+//     }
 
-    void OnDataReceived(string message)
-    {
-        message = message.Trim();  // 改行などを除去
-        color = int.Parse(message);
+//     void OnDataReceived(string message)
+//     {
+//         message = message.Trim();  // 改行などを除去
+//         color = int.Parse(message);
 
-        Debug.Log($"Received: {message}");
-    }
-}
+//         Debug.Log($"Received: {message}");
+//     }
+// }
